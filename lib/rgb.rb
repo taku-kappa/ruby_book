3460081a9b4bc319c90c9d15a3221001868ba12b
taku-kappa <168840294+taku-kappa@users.noreply.github.com>
2024-10-30 04:56:13 +0000

    Initial Commit

---
+++ lib/rgb.rb
@@ -0,0 +1,42 @@
+# def to_hex(r, g, b)
+#   [r, g, b].sum('#') do |n|
+#     n.to_s(16).rjust(2, '0')
+#   end
+# end
+
+# puts to_hex(250, 200, 255)
+
+
+# def to_ints(hex)
+#   r, g, b = hex[1..2], hex[3..4], hex[5..6]
+
+#   [r, g, b].map do |n|
+#     n.hex
+#   end
+# end
+
+# p to_ints('#fff500')
+
+
+# e = 100, 200, 300
+# puts e
+
+# def a
+#   numbers = [1, 2, 3, 4, 5]
+#     rc = []
+#   numbers.each do |n|
+#     if n.odd?
+#     rc.push(n)
+#     end
+#   end
+#   rc
+# end
+
+# pp a
+
+
+currencies = { :japan => 'yen', :us => 'dollar', :india => 'rupee'}
+p currencies[:us]
+currencies[:italy] = 'euro'
+
+p currencies
